2a07532988985a816a1c72c7536142d532779cd6
Samuel Luna <samuel@DESKTOP-0KN4OV2.>
2025-03-23 17:52:41 -0300

    apresentação dos posts e criação de component para os posts

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class MainController extends Controller
+{
+    public function index() : View
+    {
+        $posts = Post::with('user')->get();
+        return view('dashboard', ['posts' => $posts]);
+    }
+}
